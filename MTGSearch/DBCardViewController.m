2f82539ded42d3932e61cc83c68c88032c4af36a
Daniele Bottillo <dbottillo@gmail.com>
2016-08-26 16:40:03 +0100

    db updated

--- MTGSearch/DBCardViewController.m
+++ MTGSearch/DBCardViewController.m
@@ -119,12 +119,12 @@
     if (showImage){
         cardImage.image = nil;
         NSString *url;
-        if (card.getNumber > 0 && card.setCode.length > 0){
+        if (card.getNumber > 0 && card.setCode.length > 0 && ![card.setCode isEqualToString:@"EMN"] && ![card.setCode isEqualToString:@"EMA"]){
             url= [NSString stringWithFormat:@"http://magiccards.info/scans/en/%@/%d.jpg", [card setCode].lowercaseString, card.getNumber];
         } else {
             url= [NSString stringWithFormat:@"http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=%d&type=card", [card getMultiverseId]];
         }
-        NSLog(@"url: %@",url);
+        //NSLog(@"url: %@",url);
         NSURLRequest *urlRequest = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
         AFHTTPRequestOperation *requestOperation = [[AFHTTPRequestOperation alloc] initWithRequest:urlRequest];
         requestOperation.responseSerializer = [AFImageResponseSerializer serializer];
